93219c6e2929680814dcaea449a1f85ff8e6f64e
Benjamin Henke <benjaminmhenke@gmail.com>
2021-08-21 07:36:33 -0500

    Created store. Added reducers

--- src/index.js
+++ src/index.js
@@ -4,5 +4,21 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import {createStore, combineReducers, applyMiddleWare} from 'redux';
+import {Provider} from 'react-redux';
+import logger from 'redux-logger';
+
+// Store
+const storeInstance = createStore(combineReducers({
+    // Add reducers as needed
+    feelingReducer,
+    contentReducer,
+    supportReducer,
+    commentReducer
+}),
+applyMiddleWare(logger)
+);
+
+ReactDOM.render(<App store={storeInstance}/>, document.getElementById('root'));
 registerServiceWorker();
